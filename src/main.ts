9e8e0d33215f0a27ca6c2f78465fd4a8bb425e64
Ngineer 420 <ngineer420@gmail.com>
2023-11-23 14:32:30 -0700

    Hello, Stimulus! :wave:

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "@hotwired/turbo"
+
+import { Application } from "@hotwired/stimulus"
+
+import HelloController from "./controllers/hello_controller"
+
+declare global {
+  interface Window {
+    Stimulus:any;
+  }
+}
+
+window.Stimulus = Application.start()
+
+window.Stimulus.register("hello", HelloController)
