4e25485dcccaee2a73aa58fbc2f20a64edc9c9fd
Max Engine <ngineer420@gmail.com>
2023-11-25 19:39:32 -0700

    Updated output TurboFrame to append instead of replace
    Via turbo:before-frame-render event binding. Additionally simplifies #goTo by making dirTarget an anchor

--- src/controllers/terminal_controller.ts
+++ src/controllers/terminal_controller.ts
@@ -5,63 +5,53 @@ export default class extends Controller<HTMLElement> {
 
   declare readonly cliTarget: HTMLInputElement
   declare readonly cliTargets: HTMLInputElement[]
-  declare readonly dirTarget: HTMLElement
-  declare readonly dirTargets: HTMLElement[]
+  declare readonly dirTarget: HTMLAnchorElement
+  declare readonly dirTargets: HTMLAnchorElement[]
   declare readonly hasCliTarget: boolean
   declare readonly hasDirTarget: boolean
   declare readonly hasOutputTarget: boolean
   declare readonly outputTarget: HTMLElement
   declare readonly outputTargets: HTMLElement[]
 
+  appendOutput(ev: any) {
+    ev.detail.render = (currentElement: HTMLElement, newElement: HTMLElement) => {
+      while (newElement.children.length > 0) {
+        currentElement.appendChild(newElement.children[0])
+      }
+    }
+  }
+
   handle404(ev: any) {
     if (ev.detail.fetchResponse.response.status == 404) {
       ev.preventDefault()
-      this.clear()
 
-      let url = new URL(ev.detail.fetchResponse.response.url)
-
-      let args = url.pathname.replace('/index.turbo_frame.html', '')
-                    .split('/').filter(String)
-
-      let cmd = args.shift()
-
-      if (args.length > 0) {
-        cmd += ` /${args.join("/")}`
-      }
-
-      this.outputTarget.innerHTML = 
-        `Command/path not found: <strong>${cmd}</strong><br/>`
+      this.outputTarget.innerHTML += 
+        "<div><strong>Command/path not found</strong></div>"
     }
   }
 
   submit(ev: any) {
     ev.preventDefault()
-    let args = ev.target.value.split(" ")
-    let cmd = args.shift()
-    let path = this.path(args.join(" "))
-    let frame = cmd == 'cd' ? 'input' : 'output'
 
-    this.goTo(cmd + path, frame)
+    this.outputTarget.innerHTML += 
+      `<div>${this.cwdTarget.text} ${ev.target.value}</div>`
+      
+    this.goTo(ev.target.value)
   }
 
   private basePath() {
     return this.dirTarget.dataset?.dir || '/'
   }
 
-  private clear() {
+  private goTo(val: string) {
+    let args = val.split(" ")
+    let cmd = args.shift()
+
     this.cliTarget.value = ""
-    this.outputTarget.innerHTML = ""
-  }
-
-  private goTo(path: string, target: string) {
-    let anchor = document.createElement('a')
-    anchor.href = path
-    anchor.dataset.turboFrame = target
-
-    this.element.appendChild(anchor)
-    this.clear()
-
-    anchor.click()
+    this.dirTarget.href = `${document.baseURI}/${cmd}` 
+                          + this.path(args.join(" "))
+    this.dirTarget.dataset.turboFrame = cmd == 'cd' ? 'input' : 'output'
+    this.dirTarget.click()
   }
 
   private path(val: string) {
