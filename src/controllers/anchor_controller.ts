064bba28d6e33ccb85b714cdebb2edcdca2707a5
Max Engine <ngineer420@gmail.com>
2023-12-04 14:46:11 -0700

    Created /bin/source-control

---
+++ src/controllers/anchor_controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller<HTMLElement> {
+	connect() {
+    this.element.click()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "@hotwired/turbo"
 
 import { Application } from "@hotwired/stimulus"
 
+import AnchorController from "./controllers/anchor_controller"
 import TerminalController from "./controllers/terminal_controller"
 
 declare global {
@@ -12,4 +13,5 @@ declare global {
 
 window.Stimulus = Application.start()
 
+window.Stimulus.register("anchor", AnchorController)
 window.Stimulus.register("terminal", TerminalController)
